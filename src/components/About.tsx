619714764cbcb92f6ab1b66919f63d29a05ac72c
BBocho8 <teambbocho@gmail.com>
2024-02-27 13:48:26 +0100

    Links in About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,26 +6,26 @@ const About = () => {
 	return (
 		<section
 			id="about"
-			className="mx-auto flex flex-col items-center justify-center p-8 gap-y-8 h-hero0 nav:h-hero1 nav2:h-hero2  max-w-xl"
+			className="flex flex-col items-center justify-center max-w-xl p-8 mx-auto gap-y-8 h-hero0 nav:h-hero1 nav2:h-hero2"
 		>
 			<div className="avatar">
-				<div className="w-20 nav:w-28 hero:w-32 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+				<div className="w-20 rounded-full nav:w-28 hero:w-32 ring ring-primary ring-offset-base-100 ring-offset-2">
 					<img src={img} />
 				</div>
 			</div>
 			<div className="flex flex-col gap-y-1 hero:gap-y-2">
-				<h1 className="tracking-tight text-primary font-semibold text-3xl hero:text-6xl text-center">
+				<h1 className="text-3xl font-semibold tracking-tight text-center text-primary hero:text-6xl">
 					Brice Braquin
 				</h1>
-				<p className="text-right text-white font-medium text-sm hero:text-base">
+				<p className="text-sm font-medium text-right text-white hero:text-base">
 					Web Developer
 				</p>
-				<div className="flex gap-x-2 justify-end">
+				<div className="flex justify-end gap-x-2">
 					<a
-						href="#"
+						href="https://github.com/BBocho8/"
 						target="_blank"
 						rel="noreferrer"
-						className=" btn btn-xs hero:btn-sm btn-primary "
+						className=" btn btn-xs hero:btn-sm btn-primary"
 					>
 						<div className="block hero:hidden">
 							<FaGithub size={15} />
@@ -35,10 +35,10 @@ const About = () => {
 						</div>
 					</a>
 					<a
-						href="#"
+						href="https://www.linkedin.com/in/bricebraquin/"
 						target="_blank"
 						rel="noreferrer"
-						className=" btn btn-xs hero:btn-sm btn-primary "
+						className=" btn btn-xs hero:btn-sm btn-primary"
 					>
 						<div className="block hero:hidden">
 							<FaLinkedinIn size={15} />
@@ -50,12 +50,12 @@ const About = () => {
 				</div>
 			</div>
 			<div className="flex flex-col gap-y-4">
-				<p className="font-light hero:font-medium text-white text-center">
+				<p className="font-light text-center text-white hero:font-medium">
 					Hey there, I'm Brice, and I thrive on creating user-friendly,
 					captivating websites and web applications. It's all about the cool
 					stuff, in a nutshell.
 				</p>
-				<p className="font-light hero:font-medium text-white text-center">
+				<p className="font-light text-center text-white hero:font-medium">
 					When I'm not immersed in projects, you'll find me out on the field
 					playing football or creating content for my social media channels.
 					Additionally, I'm always eager to dive into the latest web
@@ -65,7 +65,7 @@ const About = () => {
 
 			<a href="#works">
 				<button className="btn hero:btn-lg btn-primary">
-					<p className="flex gap-x-4 justify-center items-center">
+					<p className="flex items-center justify-center gap-x-4">
 						SEE MY PROJECTS
 						<span className="animate-bounce">
 							<FaAngleDoubleDown size={23} />
